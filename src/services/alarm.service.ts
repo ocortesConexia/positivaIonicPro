85daf3af0ee8300cd5a79b508bf45d336b138a46
Camilo Rodriguez <ccastellanos@conexia.com>
2018-04-25 06:43:40 -0500

    alarms

--- src/services/alarm.service.ts
+++ src/services/alarm.service.ts
@@ -29,18 +29,42 @@ export class AlarmService {
   }
 
   save () {
-    this.$notifications.schedule([
+    const offset = 0//1000 * 60 * 60 * 2
+
+    const alarms = [
       {
+        id: 1,
         title: 'Cita de control',
-        text: 'Su cita es en 1 minuto, apúrese',
-        trigger: { at: new Date(+new Date() + 3600) }
+        text: 'Su cita es en 2 horas',
+        trigger: { at: +this.alarms.control - offset }
       },
       {
+        id: 2,
         title: 'Entrega de medicamentos',
-        text: 'La entrega de medicamentos es en (no sé)',
-        trigger: { at: new Date(+new Date() + 4800) }
+        text: 'La entrega de medicamentos es en 2 horas',
+        trigger: { at: +this.alarms.delivery - offset }
       }
-    ])
+    ]
+
+    this.$notifications.on('trigger')
+      .subscribe((...args) => {
+        console.log(args)
+      })
+
+    const SKIP_INDEXES = 2
+
+    alarms.push(
+      ...this.alarms.medicines.map((medicine, index) => {
+        return {
+          id: SKIP_INDEXES + index,
+          title: 'Toma de medicamento',
+          text: `La toma de su medicamento ${medicine.type} es en 5 minutos`,
+          trigger: { at: +medicine.from - offset }
+        }
+      })
+    )
+
+    this.$notifications.update(alarms as any)
 
     localStorage.setItem('alarms', JSON.stringify(this.alarms))
   }
